36cd02d1aa50e17b5097a29bcb29f3a3e30731c9
Olgiati Davide <davide.olgiati@hotmail.it>
2017-03-12 17:23:44 +0100

    testing script

---
+++ push_steg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+g++ -g /Users/davide/Documents/Steg/test/steg.cpp -o /Users/davide/Documents/Steg_test/dojo.test -Wall -ansi
+git add .
+git status
+read -p "Commit description: " desc
+git commit -m "$desc"
+git push origin master
+cd ../Steg_test/
+exec dojo.test
