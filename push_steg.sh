48eb6b3797f68565b5a834115c09cf0eceef715c
Olgiati Davide <davide.olgiati@hotmail.it>
2018-12-27 16:10:06 +0100

    Delete push_steg.sh

--- push_steg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-g++ -g /Users/davide/Documents/Steg/test/steg.cpp -o /Users/davide/Documents/Steg_test/dojo.test -Wall -ansi
-git add .
-git status
-read -p "Commit description: " desc
-git commit -m "$desc"
-git push origin master
